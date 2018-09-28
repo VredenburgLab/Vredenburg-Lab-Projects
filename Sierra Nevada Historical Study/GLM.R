c25eb93b31f3071dae743bcace597696be9bdc63
Hasan Sulaeman <hasanregius@gmail.com>
2018-09-28 14:53:43 -0700

    whatevs

--- Sierra Nevada Historical Study/GLM.R
+++ Sierra Nevada Historical Study/GLM.R
@@ -14,12 +14,11 @@ library(DAAG)
 library(tidyr)
 library(ggpubr)
 library(Hmisc)
-library(reshape2)
-data = read.csv("Samrawdata_final.csv")
+data = read.csv("/Users/hasansulaeman/Dropbox/Sam's Sierra Bd work/Data/samsara.csv")
 pre = subset(data, data$Year > 1969)
 post = subset(data, data$Year < 1969)
 
-# Making a summary table
+# Making a summary table ----
 sum.table = dcast(data=data,Decade~BdStatus,value.var='Decade',fun.aggregate=length)
 names(sum.table)[c(1,2,3)] = c('Decade','neg','pos')
 sum.table$n = sum.table$neg + sum.table$pos
@@ -35,7 +34,7 @@ sum.table$upper=sum.table$upper*100
 sum.table$prob =  dbinom(0, sum.table$n,.11)
 write.csv(sum.table,'DecadeTable.csv')
 
-# Fetching Worlcclim and Anthropogenic Data from Venter et al. ----
+# Fetching Anthropogenic Data from Venter et al. and NHD data ----
 ## Venter et al. Data ----
 ## Important: You first have to download the files at the repository specified in Venter et al.
 ## Change the directory below to the .tif file you downloaded from the repository
@@ -55,44 +54,59 @@ sp = SpatialPointsDataFrame(coords = x, data=x,
      proj4string=CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"))
 ay = raster::extract(anthro, sp)
 data = cbind.data.frame(data, ay)
-## Worldclim Bioclimatic Variable Data ----
+
+## NHD Closest water body ----
+data$distwater = 0
+data$catwater = 0
+for(i in 1:nrow(data)){
+  data[i,]$distwater = min(data[i,57:66])       
+  for (j in 57:66){                           # Change column numbers here
+    if(data[i,j] == data[i,]$distwater){
+      data[i,]$catwater = names(data)[j]
+    }
+  }
+}
+
+write.csv(data, "samsara.csv")
 
 # Multicollinearity Reduction and Covariate Scaling ----
-cor = rcorr(as.matrix(data[,29:55]) , type= "pearson")
-cor.pre = rcorr(as.matrix(pre[,29:55]) , type= "pearson")
-cor.post = rcorr(as.matrix(post[,29:55]) , type= "pearson")
-## From cor$r we see that, for all three, Bio 1, 3, 7, 12, 15 
+cor = rcorr(as.matrix(data[,c(6,29:41)]) , type= "pearson")
+cor.pre = rcorr(as.matrix(pre[,c(6,29:41)]) , type= "pearson")
+## From cor$r we see that, for all three, only elev and temps are highly correlated
 ## Now let's scale the covariates
-data[,c(29,31,35,40,43,48:55)] = scale(data[,c(29,31,35,40,43,48:55)])
+data[,c(29:36,38,40:41)] = scale(data[,c(29:36,38,40:41)])
 
 # Run the models ----
 ## Logistic Regression for Overall
-lm = glm(BdStatus ~ bio1 + bio3 + bio7 + bio12 + bio15 + Elevation.m. +
-           HFP2009 + croplands2005 + Built2009 + Popdensity2010 + Roads + Railways +
-           Pasture2009, family = "binomial", data = data)
+lm = glm(BdStatus ~ distwater + factor(catwater) + Roads + ppt +
+         tmean + Popdensity2010 + croplands2005 + Built2009 + Species +
+         Popdensity2010 + Railways + Pasture2009 + HFP2009, 
+         family = "binomial", data = data)
 step = stepAIC(lm, direction = "both")
 model.comparison = step$anova
-final.lm = glm(BdStatus ~ bio1 + bio3 + bio7 + bio12 + bio15 + Elevation.m. + HFP2009 + 
-               Built2009 + Popdensity2010, family = "binomial", data = data)
+final.lm = glm(BdStatus ~ Roads + ppt + tmean + Popdensity2010 + croplands2005 + 
+               Built2009 + Railways + HFP2009, family = "binomial", data = data)
 summary(final.lm)
 ## 5-fold Cross-validation
 kfold = cv.binary(final.lm, nfolds=100) # 5 fold cross-validation
 
 ## Logistic Regression for pre-1970
-lm = glm(BdStatus ~ bio1 + bio3 + bio7 + bio12 + bio15 + Elevation.m. +
-         HFP2009 + croplands2005 + Built2009 + Popdensity2010 + Roads + Railways +
-         Pasture2009, family = "binomial", data = pre)
-step = stepAIC(lm, direction = "both")
-model.comparison = step$anova
-final.lm = glm(BdStatus ~ bio7 + bio12 + bio15 + Elevation.m. + HFP2009,
-               family = "binomial", data = pre)
+lm = glm(BdStatus ~ distwater + factor(catwater) + Roads + ppt +
+         tmean + Popdensity2010 + croplands2005 + Built2009 + Species +
+         Popdensity2010 + Railways + Pasture2009 + HFP2009, 
+         family = "binomial", data = pre)
+step1 = stepAIC(lm, direction = "both")
+model.comparison = step1$anova
+final.lm = glm(BdStatus ~ distwater + Built2009 + Species 
+               + Railways + HFP2009,family = "binomial", data = pre)
 summary(final.lm)
+
 ## 5-fold Cross-validation
 kfold = cv.binary(final.lm, nfolds=100) # 5 fold cross-validation
 
 ## Logistic Regression for post-1970
-lm = glm(BdStatus ~ bio1 + bio3 + bio7 + bio12 + bio15 + Elevation.m. +
-           HFP2009 + croplands2005 + Built2009 + Popdensity2010 + Roads + Railways +
+lm = glm(BdStatus ~ distwater + factor(catwater) + ppt + tmean + HFP2009 +
+           croplands2005 + Built2009 + Popdensity2010 + Roads + Railways +
            Pasture2009, family = "binomial", data = post)
 step = stepAIC(lm, direction = "both")
 model.comparison = step$anova
@@ -100,5 +114,9 @@ final.lm = glm(BdStatus ~ bio1 + bio3 + bio7 + bio12 + bio15 + Elevation.m. + HF
                  Built2009 + Popdensity2010, family = "binomial", data = )
 summary(final.lm)
 ## 5-fold Cross-validation
-kfold = cv.binary(final.lm, nfolds=100) # 5 fold cross-validation
+kfold = cv.binary(step1, nfolds=100) # 5 fold cross-validation
 
+# Table for Publication ----
+library(sjPlot)
+tab_model(step, step1, show.aic = T, show.ci = F, show.est = T, 
+          show.fstat = T, show.obs = F, show.r2 = F)
